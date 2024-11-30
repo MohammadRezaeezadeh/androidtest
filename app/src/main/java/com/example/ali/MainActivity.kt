7fb6acf7ba6c03c0512863f9abab31e88ce4075f
MohammadRezaeezadeh <marzam6484@gmail.com>
2024-11-30 10:22:42 +0330

    hello world

---
+++ app/src/main/java/com/example/ali/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.ali
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ali.ui.theme.AliTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AliTheme {
+                Button(
+                    modifier = Modifier
+
+                        .fillMaxHeight()
+
+                    ,onClick = {}) {
+
+                    Text("salam")
+                }
+
+            }
+        }
+    }
+
+}
